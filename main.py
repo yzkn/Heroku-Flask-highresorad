6df19243e8e3a7ac31f403b7f315b9c69e2536e5
YA <YA-androidapp@users.noreply.github.com>
2019-12-05 00:03:15 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,18 +7,39 @@ import datetime
 import logging
 import os
 
-from flask import Flask
+from flask import Flask, request
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
+# const
+DEFAULT_LAT = 35.681236
+DEFAULT_LNG = 139.767125
+
+
+def get_rainfall(lat, lng):
+    return 0  # TODO
+
+
+def is_numeric(n):
+    try:
+        float(n)
+    except ValueError:
+        return False
+
 
 @app.route('/nowcast', methods=['GET'])
-def get():
-    #TODO
-    rainfall = 0
+def get_nowcast():
+    lat = request.args.get('lat', DEFAULT_LAT)
+    lng = request.args.get('lng', DEFAULT_LNG)
+
+    if False == is_numeric(lat) or False == is_numeric(lng):
+        return '', 404
+
+    # TODO
+    rainfall = get_rainfall(lat, lng)
 
     res = 'timestamp={}, RAINFALL={}'.format(
         datetime.datetime.utcnow() + datetime.timedelta(hours=9), rainfall)
---
+++ utility.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2019 YA-androidapp(https://github.com/YA-androidapp) All rights reserved.
+
+import datetime
+from selenium import webdriver
+
+
+def get_rainfall(lat, lng):
+    pass
